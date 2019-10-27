840f46fb84f7f1a002179e092f48ba16c600b3bc
DaryaYa <lyalyaka@mail.ru>
2019-10-27 13:19:16 +0300

    draw

---
+++ canvas.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('#draw');
+const ctx = canvas.getContext('2d');
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = '30';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+    if (!isDrawing) return;
+
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+    // lastX = e.offsetX;
+    //lastY = e.offsetY;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
