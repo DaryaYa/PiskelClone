b7c09534bd30ae06f5853f708ad9d7b3ff80f14b
DaryaYa <lyalyaka@mail.ru>
2019-10-29 13:13:29 +0300

    json32x32

--- canvas.js
+++ canvas.js
@@ -40,7 +40,7 @@ function drawPic() {
     img.onload;
     ctx.drawImage(img, 0, 0, 512, 512);
 }
-
+/*
 let dataFourByFour = [
     ["00BCD4", "FFEB3B", "FFEB3B", "00BCD4"],
     ["FFEB3B", "FFC107", "FFC107", "FFEB3B"],
@@ -59,6 +59,15 @@ function fourByFour() {
             ctx.fillRect(i * scale, j * scale, (i + 1) * scale, (j + 1) * scale);
         }
     }
+}*/
+function fourByFour(frame, ctx) {
+    let scale1 = 512 / 4;
+    frame.forEach((raw, i) => {
+        raw.forEach((column, j) => {
+            ctx.fillStyle = '#' + column;
+            ctx.fillRect(j * scale1, i * scale1, (j + 1) * scale1, (i + 1) * scale1);
+        });
+    });
 }
 
 function draw32x32(frame, ctx) {
@@ -73,3 +82,7 @@ function draw32x32(frame, ctx) {
 let draw32 = document.querySelector('.draw32x32');
 draw32.onclick = () => fetch('./data/32x32.json')
     .then(response => response.json().then(json => draw32x32(json, ctx)));
+
+let draw4x4 = document.querySelector('.draw4x4');
+draw4x4.onclick = () => fetch('./data/4x4.json')
+    .then(response => response.json().then(json => fourByFour(json, ctx)));
